28f3bde7ed394d6bb6f34bde08d8f5de1a19ec5b
HWS <15757135983@163.com>
2017-12-19 15:38:45 +0800

    1.数字输入框组件

--- src/common/inputnumber/InputNumber.ts
+++ src/common/inputnumber/InputNumber.ts
@@ -1,20 +1,56 @@
-import {Component, OnInit, Input, Output, EventEmitter} from "@angular/core";
+import {Component, OnInit, Input, Output, EventEmitter, forwardRef} from "@angular/core";
+import {NG_VALUE_ACCESSOR, ControlValueAccessor} from "@angular/forms";
+import {isUndefined} from "util";
 @Component({
   selector:'hk-input-number',
-  templateUrl:'InputNumber.html'
+  template:`
+    <input type="text" class="hk_input" [(ngModel)]="value">
+  `,
+  providers:[{
+    provide: NG_VALUE_ACCESSOR,
+    useExisting: forwardRef(() => InputNumber),
+    multi: true
+  }]
 })
 
-export class InputNumber implements OnInit{
+export class InputNumber implements OnInit,ControlValueAccessor {
 
-  @Input() val:number;
+  innerValue:number=0;
+  onTouchedCallback(){}
+  onChangeCallback(_: any){}
   @Input() max:number;
   @Input() min:number;
-  @Output() hkchange:EventEmitter<any> = new EventEmitter();
   constructor(){}
   ngOnInit(){}
 
-  changeValue(){
-    this.hkchange.emit(Number(this.val));
+  get value(): any {
+    return Number(this.innerValue);
   }
+
+  set value(v: any) {
+    v=Number(v);
+    if (v !== this.innerValue) {
+      this.innerValue = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  writeValue(value: any) {
+    if(isUndefined(value)){
+      value = 0;
+    }
+    value=Number(value);
+    if (value !== this.innerValue) {
+      this.innerValue = value;
+    }
+  }
+
+  registerOnChange(fn: any) {
+    this.onChangeCallback = fn;
+  }
+  registerOnTouched(fn: any) {
+    this.onTouchedCallback = fn;
+  }
+
 
 }
