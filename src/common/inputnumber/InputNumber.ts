c8a4c798cab01ba035af674228e90dcf8dd27410
HWS <15757135983@163.com>
2017-12-19 14:29:16 +0800

    1.数字输入框组件

--- src/common/inputnumber/InputNumber.ts
+++ src/common/inputnumber/InputNumber.ts
@@ -9,12 +9,12 @@ export class InputNumber implements OnInit{
   @Input() val:number;
   @Input() max:number;
   @Input() min:number;
-  @Output() change:EventEmitter<any> = new EventEmitter();
+  @Output() hkchange:EventEmitter<any> = new EventEmitter();
   constructor(){}
   ngOnInit(){}
 
   changeValue(){
-    this.change.emit(Number(this.val));
+    this.hkchange.emit(Number(this.val));
   }
 
 }
